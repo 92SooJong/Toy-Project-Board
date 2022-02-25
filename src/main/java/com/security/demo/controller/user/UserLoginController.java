684832a2d3b752ac9d89defe4babfb18a07358a7
92SooJong <hsz3855@gmail.com>
2022-02-26 01:40:35 +0900

    thymeleaf classpath 수정

--- src/main/java/com/security/demo/controller/user/UserLoginController.java
+++ src/main/java/com/security/demo/controller/user/UserLoginController.java
@@ -1,8 +1,12 @@
 package com.security.demo.controller.user;
 
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.Resource;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 @RequestMapping
@@ -10,6 +14,7 @@ public class UserLoginController {
 
     @GetMapping({"/login" ,"/"})
     public String getLoginView(){
+
         return "/login/login";
     }
 
