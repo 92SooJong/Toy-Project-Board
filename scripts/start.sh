c923ca0cfdaa05f6b62e45baea7a47e97d9b406b
92SooJong <hsz3855@gmail.com>
2022-03-01 01:41:14 +0900

    실행 스크립트 수정

--- scripts/start.sh
+++ scripts/start.sh
@@ -3,9 +3,16 @@
 JAR_PATH=/home/ec2-user/toy-project-board/deploy
 PROJECT_PATH=/home/ec2-user/toy-project-board/deploy/project
 
+
+echo "> change gradlew Permission to Excutable"
+sudo chmod u+x $PROJECT_PATH/gradlew
+
+echo "> Start Build"
+
+.$PROJECT_PATH/gradlew build
+
 echo "> copy Jar"
 cp $PROJECT_PATH/build/libs/*jar $JAR_PATH/
 
 echo "> Run Jar"
-
 nohup java -jar -Dspring.config.location=/home/ec2-user/toy-project-board/application-db.yml $JAR_PATH/demo-0.0.1-SNAPSHOT.jar > $JAR_PATH/nohup.out 2>&1 &
