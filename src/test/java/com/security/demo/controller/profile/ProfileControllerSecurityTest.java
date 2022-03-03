09d9bfda59584565b2bddb7dbf258e1751ec76b6
92SooJong <hsz3855@gmail.com>
2022-03-03 23:50:24 +0900

    profile Security 관련 테스트 코드 작성중

---
+++ src/test/java/com/security/demo/controller/profile/ProfileControllerSecurityTest.java
@@ -0,0 +1,37 @@
+package com.security.demo.controller.profile;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.boot.web.server.LocalServerPort;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.mock.env.MockEnvironment;
+import org.springframework.test.context.ActiveProfiles;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+class ProfileControllerSecurityTest {
+
+    @LocalServerPort
+    private int port;
+
+    @Autowired
+    private TestRestTemplate testRestTemplate;
+
+    @Test
+    public void canAccessToProfileWithoutAuthorization(){
+        String expected = "default";
+
+        ResponseEntity<String> response = testRestTemplate.getForEntity("/profile", String.class);
+        assertThat(response.getStatusCode()).isEqualTo(HttpStatus.OK);
+        assertThat(response.getBody()).isEqualTo(expected);
+
+
+    }
+
+
+
+}
