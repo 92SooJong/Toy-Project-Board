cc9a504f157eac677c49e6f2fd244d1beab3015c
92SooJong <hsz3855@gmail.com>
2022-03-06 23:17:58 +0900

    BeforeInstall 단계 삭제

--- scripts/stop.sh
+++ scripts/stop.sh
@@ -4,7 +4,7 @@ ABSPATH=$(readlink -f $0)
 ABSDIR=$(dirname "$ABSPATH")
 source "${ABSDIR}"/profile.sh
 
-IDLE_PORT=$(find_idle_port)
+IDLE_PORT="$(find_idle_port)"
 
 echo "> $IDLE_PORT에서 구동 중인 애플리케이션 pid 확인"
 IDLE_PID=$(lsof -ti tcp:"${IDLE_PORT}")
