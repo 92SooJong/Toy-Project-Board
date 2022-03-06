42865abf9742b5bcac7f714cd1b62d1347378b87
92SooJong <hsz3855@gmail.com>
2022-03-06 23:31:24 +0900

    echo 추가

--- scripts/stop.sh
+++ scripts/stop.sh
@@ -4,6 +4,9 @@ ABSPATH=$(readlink -f $0)
 ABSDIR=$(dirname "$ABSPATH")
 source "${ABSDIR}"/profile.sh
 
+echo "Profile을 풀러옵니다. ${ABSDIR}/profile.sh"
+
+
 IDLE_PORT="$(find_idle_port)"
 
 echo "> $IDLE_PORT에서 구동 중인 애플리케이션 pid 확인"
