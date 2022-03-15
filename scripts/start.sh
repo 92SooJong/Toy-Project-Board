482e9c398f4f0b3456397c22d31d4195fc045d8d
92SooJong <hsz3855@gmail.com>
2022-03-15 23:26:09 +0900

    -p 옵션 원복

--- scripts/start.sh
+++ scripts/start.sh
@@ -40,4 +40,4 @@ echo ">JAR_FILE==> $JAR_FILE"
 
 sudo docker build -t toy-project-board:sample --build-arg JAR_FILE=$JAR_FILE $DOCKER_PATH
 
-sudo docker run -d -p 80:${IDLE_PORT} --name ${IDLE_PROFILE} -e "IDLE_PROFILE=${IDLE_PROFILE}" toy-project-board:sample
+sudo docker run -d -p ${IDLE_PORT}:${IDLE_PORT} --name ${IDLE_PROFILE} -e "IDLE_PROFILE=${IDLE_PROFILE}" toy-project-board:sample
