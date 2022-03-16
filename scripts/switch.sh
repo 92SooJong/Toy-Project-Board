282e8afdc419b8d14cb9349c87ebc4b80156f076
92SooJong <hsz3855@gmail.com>
2022-03-16 11:24:01 +0900

    nginx 재시작 스크립트 수정

--- scripts/switch.sh
+++ scripts/switch.sh
@@ -12,6 +12,6 @@ function switch_proxy(){
   echo "set \$service_url http://127.0.0.1:${IDLE_PORT};" | sudo tee /etc/nginx/conf.d/service-url.inc
 
   echo "> 엔진엑스 재시작"
-  sudo systemctl nginx reload
+  sudo service nginx reload
 
 }
